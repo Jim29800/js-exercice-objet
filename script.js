4565294ccf66c800d24dd05ed6d7b9547b9b087d
Jim29800 <jbouvier1173@gmail.com>
2017-08-07 14:56:08 +0200

    amelioration addEleve

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function eleve(prenom, nom, sexe, age) {
-    this.id = 0;
+function eleve(id, prenom, nom, sexe, age) {
+    this.id = id;
     this.prenom = prenom;
     this.nom = nom;
     this.sexe = sexe;
@@ -14,22 +14,20 @@ function eleve(prenom, nom, sexe, age) {
     }
 }
 
-
-var curIdEleve = 0
-
 function classe() {
-    this.curIdEleve = curIdEleve;
+    this.curIdEleve = 1;
     this.eleves = [];
-
-    this.addEleve = function (ajout) {
-        this.eleves.push(ajout)
-        ajout.id = curIdEleve
-        curIdEleve++
+    // methode ajout eleve
+    this.addEleve = function (prenom, nom, sexe, age ) {
+        var nouveau =  new eleve(this.curIdEleve, prenom, nom, sexe, age )
+        this.eleves.push(nouveau)
+        this.curIdEleve++
     }
-
+    //methode supprimer eleve
     this.removeEleve = function (id) {
         this.eleves.splice(id, 1)
     }
+    //methode compte eleve
     this.compteEleve = function (sexe) {
         var nbEleve = this.eleves.length;
         var masculin = 0;
@@ -53,6 +51,7 @@ function classe() {
             return "erreur d'argument"
         }
     }
+    //methode affiche les eleves
     this.afficheEleves = function () {
         console.log("cette classe contient les eleves suivant : ")
         for (var i = 0; i < this.eleves.length; i++) {
@@ -60,6 +59,7 @@ function classe() {
         }
 
     }
+    //methode moyenne age eleves
     this.moyenneAgeEleves = function () {
         var total = 0;
         var nbEleve = this.eleves.length;
@@ -68,10 +68,7 @@ function classe() {
         }
         return total / nbEleve
     }
-
-
-
-
+    //methode affiche stat de la classe
     this.afficheStatClasse = function () {
         console.log("cette classe contient : " + this.compteEleve("a") + " eleves")
         console.log("cette classe contient : " + this.compteEleve("m") + " hommes")
@@ -79,14 +76,10 @@ function classe() {
         console.log("La moyenne d'age des eleves est de : " + this.moyenneAgeEleves() + " ans")
     }
 }
-var jim = new eleve("jim", "bo", "m", 30);
-var leo = new eleve("leo", "sub", "f", 20);
+
 var classe1 = new classe()
-classe1.addEleve(jim)
-classe1.addEleve(leo)
-// jim.affiche()
-// leo.affiche()
-// classe1.affiche()
-// classe1.compteEleve("f")
+classe1.addEleve("jim", "bou", "m", 30)
+classe1.addEleve("leo", "sub", "f", 20)
+classe1.addEleve("chris", "topher", "m", 40)
 classe1.afficheEleves()
 classe1.afficheStatClasse()
